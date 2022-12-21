2281497e2b4758491fd3c03931ad1b00324d1815
Bella Ranalli <bela.ranalli@gmail.com>
2022-12-20 22:25:13 -0300

    segundo ejercicio

--- hola_mundo.js
+++ hola_mundo.js
@@ -1,1 +1,19 @@
+//primer ejercicio
 console.log("hola mundo");
+
+
+//segundo ejercicio
+const datosPersonales = {
+    nombre: "Bella",
+    edad: 33,
+    desarrollador: true,
+    nacimiento: new Date(1989, 8, 12),
+      libro: {
+      titulo: "Inventado",
+      autor: "Desconocido",
+      año: 1950,
+        },
+  
+  };
+  
+  console.log(datosPersonales)
